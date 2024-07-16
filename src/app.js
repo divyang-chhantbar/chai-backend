5acaf1eba38214bd4be9f6027f960791432d70f7
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-07-16 19:51:49 +0530

    Added routes for healthcheck, tweets, subscriptions, videos, comments, likes, playlist, and dashboard .

--- src/app.js
+++ src/app.js
@@ -27,11 +27,28 @@ app.use(cookieParser())
 
 // routes
 import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/likes.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 // app.get("/",(req,res) => {}); // this is a route declaration
 // we used to use this earlier because through the (app) we were writing both routes and controller here but now we are using the routes and controller separately so we will use the below code to declare the routes :
 app.use("/api/v1/users",userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+
 export{app};
 
 // now we will go for more standardization of our code we are seeing that some of the code need a same wrapper code so we will add that thing to utility folder and we will use that thing in our code so that we can make our code more standardize and more readable and more maintainable .  
